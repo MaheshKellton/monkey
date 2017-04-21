7c3cf7ca460eb10303596bc9b34c8c12df0b60cc
Mahesh <mahesh.pal@kelltontech.com>
2017-04-21 16:49:20 +0530

    add constant file

---
+++ CoreData/CoreData/Constant.h
@@ -0,0 +1,13 @@
+//
+//  Constant.h
+//  CoreData
+//
+//  Created by mahesh.pal on 4/21/17.
+//  Copyright © 2017 mahesh.pal. All rights reserved.
+//
+
+#ifndef Constant_h
+#define Constant_h
+
+
+#endif /* Constant_h */
